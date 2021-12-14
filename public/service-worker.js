2ac7149b79aef2b4cdb1a033ae00ef41badf6798
James Sgarella <Jimbo8702@gmail.com>
2021-12-14 17:19:23 -0500

    manifest and service worker added

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+
+  "/icons/icon_192x192.png",
+  "/icons/icon_512x512.png",
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
